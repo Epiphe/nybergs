f6520b5f52f79a4b01fd3b01e1ef8b63a9d90702
Johan Nyberg <johannyberg@Johans-iMac.local>
2019-11-05 11:35:21 +0100

    Fixed img overflow in gallery and aria on menu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
+
+
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-  document.getElementById("menyDropdown").classList.toggle("show").setAttribute('aria-expanded','true');
-
+  document.getElementById("menyDropdown").classList.toggle("show");
+  document.getElementById("dropDownBtn").setAttribute('aria-expanded','true');
 }
 
 // Close the dropdown menu if the user clicks outside of it
@@ -10,11 +12,11 @@ window.onclick = function(event) {
   if (!event.target.matches('.menyButton')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
+    document.getElementById("dropDownBtn").setAttribute('aria-expanded','false');
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
-        this.setAttribute('aria-expanded','false')
       }
     }
   }
